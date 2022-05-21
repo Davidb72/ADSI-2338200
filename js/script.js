2ef13d49392c852176709ac43a725ddac026754b
Davidb72 <barondavid103@gmail.com>
2022-05-21 10:39:34 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,31 @@
-/*const productos = [
-    {nombre: 'Platanos', valor: 500},
-    {nombre: 'Pera', valor: 500},
-    {nombre: 'Sandia', valor: 500},
-    {nombre: 'Melon', valor: 500},
-]
-
-
-
-const formulario = document.querySelector('#formulario');
-const boton = document.querySelector('#boton');
-const resultado = document.querySelector('#resultado');
-
-// FUNCION
-const filtrar = ()=> {
-    //console.log(formulario.value);
-    resultado.innerHTML = '';
-
-    const texto = formulario.value.toLowerCase();
-    for(let producto of productos){
-        let nombre = producto.nombre.toLowerCase();
-        if(nombre.indexOf(texto) !== -1){
-            resultado.innerHTML +=  `
-            <li>${producto.nombre} - valor: ${producto.valor}</li>
-            `
-        }
+btn.onclick = function() {
+    var buscar = document.getElementById("buscar").value;
+    if (buscar == 'html') {
+        panel.style.color='#fff'
+        panel.style.textTransform = 'uppercase'
+        panel.innerHTML = "Se Encuentra";
+    }
+    else if (buscar == 'css') {
+        panel.style.color='#fff'
+        panel.style.textTransform = 'uppercase'
+        panel.innerHTML = "Se Encuentra";
+    }
+    else if (buscar == 'javascript') {
+        panel.style.color='#fff'
+        panel.style.textTransform = 'uppercase'
+        panel.innerHTML = "Se Encuentra";
+    }
+    else if (buscar == '') {
+        panel.style.color='#fff'
+        panel.style.textTransform = 'uppercase'
+        panel.innerHTML = "ERROR...";
+    }
+     
+    else {
+        panel.innerHTML = "No Existe....."
+        panel.style.color='#fff'
+        
     }
 
-    if(resultado.innerHTML === ''){
-        resultado.innerHTML +=  `
-            <li>No Existe.....</li>
-            `
-    }
 }
 
-
-boton.addEventListener('click', filtrar)*/
